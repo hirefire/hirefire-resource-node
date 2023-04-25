cde60b3efe4824892b8cbbd180a3d3efe1b8e82f
Michael van Rooijen <michael@vanrooijen.io>
2023-04-25 15:27:50 +0200

    Add Mutex class

---
+++ src/mutex.ts
@@ -0,0 +1,26 @@
+export class Mutex {
+  private locked: boolean = false
+  private readonly waiting: Array<() => void> = []
+
+  async lock (): Promise<void> {
+    while (this.locked) {
+      await new Promise<void>(resolve => this.waiting.push(resolve))
+    }
+    this.locked = true
+  }
+
+  unlock (): void {
+    this.locked = false
+    const nextResolve = this.waiting.shift()
+    nextResolve?.()
+  }
+
+  async synchronize<T> (fn: () => Promise<T>): Promise<T> {
+    await this.lock()
+    try {
+      return await fn()
+    } finally {
+      this.unlock()
+    }
+  }
+}
