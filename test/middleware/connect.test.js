5af3e191b062140ab42a6565e46470a3ee248881
Michael van Rooijen <michael@vanrooijen.io>
2023-11-11 08:08:29 +0100

    WIP

--- test/middleware/connect.test.js
+++ test/middleware/connect.test.js
@@ -23,20 +23,25 @@ describe('HireFireMiddlewareConnect', () => {
     sinon.restore()
   })
 
-  test('ignore middleware when HIREFIRE_TOKEN is not set', async () => {
+  test('pass through without HIREFIRE_TOKEN', async () => {
     HireFire.configuration.dyno('web')
     HireFire.configuration.dyno('worker', () => 5)
     const start = jest.spyOn(HireFire.configuration.web, 'start')
-    const response = await request(app)
-      .get('/')
-      .set('X-Request-Start', 1)
+    const response = await request(app).get('/').set('X-Request-Start', 1)
     expect(response.status).toBe(200)
     expect(response.text).toBe('Hello')
-    expect(start).not.toHaveBeenCalled()
     expect(HireFire.configuration.web.buffer).toEqual({})
+    expect(start).not.toHaveBeenCalled()
   })
 
-  test('pass through and handle request queue time process', async () => {
+  test('pass through without configuration', async () => {
+    process.env.HIREFIRE_TOKEN = 'SOME_TOKEN'
+    const response = await request(app).get('/').set('X-Request-Start', 1)
+    expect(response.status).toBe(200)
+    expect(response.text).toBe('Hello')
+  })
+
+  test('pass through and process web configuration', async () => {
     process.env.HIREFIRE_TOKEN = 'SOME_TOKEN'
     const now = Date.now()
     const nowTimestamp = Math.floor(now / 1000)
@@ -44,16 +49,14 @@ describe('HireFireMiddlewareConnect', () => {
     sinon.useFakeTimers(now)
     HireFire.configuration.dyno('web')
     const start = jest.spyOn(HireFire.configuration.web, 'start')
-    const response = await request(app)
-      .get('/some/other/path')
-      .set('X-Request-Start', requestStartTime)
+    const response = await request(app).get('/').set('X-Request-Start', requestStartTime)
     expect(response.status).toBe(200)
     expect(response.text).toBe('Hello')
-    expect(start).toHaveBeenCalled()
     expect(HireFire.configuration.web.buffer).toEqual({ [nowTimestamp]: [1234] })
+    expect(start).toHaveBeenCalled()
   })
 
-  test('intercept and return job queue information', async () => {
+  test('intercept and process worker configuration', async () => {
     process.env.HIREFIRE_TOKEN = 'SOME_TOKEN'
     HireFire.configuration.dyno('worker', () => 5)
     const response = await request(app).get('/hirefire/SOME_TOKEN/info')
