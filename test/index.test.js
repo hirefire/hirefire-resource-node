7244b07a2f2dde605cb037d47eb7d838ead701fc
Michael van Rooijen <michael@vanrooijen.io>
2023-11-16 20:26:03 +0100

    wip

--- test/index.test.js
+++ test/index.test.js
@@ -3,11 +3,8 @@
 const HireFire = require('../src')
 const { Configuration } = require('../src/configuration')
 
-describe('HireFire', () => {
-  test('configure yields configuration', () => {
-    let receivedConfig
-    HireFire.configure(config => { receivedConfig = config })
-    expect(receivedConfig).toBeInstanceOf(Configuration)
-    expect(receivedConfig).toBe(HireFire.configuration)
+describe('HireFire singleton', () => {
+  test('default configuration', () => {
+    expect(HireFire.configuration).toBeInstanceOf(Configuration)
   })
 })
